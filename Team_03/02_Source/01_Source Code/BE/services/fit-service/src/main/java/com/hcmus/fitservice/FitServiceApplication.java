65dff93c906f4c81797ab2c8107700cf14b374c1
TEIKHNA <anhkiet07012003@gmail.com>
2025-03-20 00:21:49 +0700

    setup: update services

---
+++ Team_03/02_Source/01_Source Code/BE/services/fit-service/src/main/java/com/hcmus/fitservice/FitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hcmus.fitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitServiceApplication.class, args);
+    }
+
+}
