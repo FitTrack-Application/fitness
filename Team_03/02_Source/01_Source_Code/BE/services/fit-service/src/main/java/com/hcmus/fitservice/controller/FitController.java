c4161626249cc4f324fbbd67b1008193f1a743ab
TEIKHNA <anhkiet07012003@gmail.com>
2025-03-20 03:16:42 +0700

    update import

--- Team_03/02_Source/01_Source_Code/BE/services/fit-service/src/main/java/com/hcmus/fitservice/controller/FitController.java
+++ Team_03/02_Source/01_Source_Code/BE/services/fit-service/src/main/java/com/hcmus/fitservice/controller/FitController.java
@@ -1,9 +1,13 @@
 package com.hcmus.fitservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class FitController {
-    
+
     @GetMapping("test")
     public String testServer() {
         return "Test";
