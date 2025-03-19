250feb7d45f364930c9df51e7eb91c02bfa0f47e
TEIKHNA <anhkiet07012003@gmail.com>
2025-03-20 00:06:34 +0700

    setup: remove files

---
+++ Team_03/02_Source/01_Source Code/BE/services/fitness-serivce/src/test/java/com/hcmus/fitnessserivce/FitnessSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmus.fitnessserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
