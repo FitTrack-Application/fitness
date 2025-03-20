d9c4a8bc0c8cf1667e7ed1728d4686789152da5c
TEIKHNA <anhkiet07012003@gmail.com>
2025-03-20 11:21:40 +0700

    setup services docker

---
+++ Team_03/02_Source/01_Source_Code/BE/services/user-service/src/main/java/com/hcmus/userservice/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.hcmus.userservice.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> testServer() {
+        return ResponseEntity.ok("User service");
+    }
+}
