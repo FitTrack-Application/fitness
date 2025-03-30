e8d730c9b993e03ec47983e2be333a224f6a963a
Tu Le <leanhtuqb1803@gmail.com>
2025-03-30 19:08:24 +0700

    Update Food model and implement get all foods api

---
+++ Team_03/02_Source/01_Source_Code/BE/services/fit-service/src/main/java/com/hcmus/fitservice/model/Food.java
@@ -0,0 +1,40 @@
+package com.hcmus.fitservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "foods")
+public class Food {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name="food_id")
+    private UUID id;
+
+    private String name;
+
+    @Column(name = "calories_per_100g")
+    private int caloriesPer100g;
+
+    @Column(name = "protein_per_100g")
+    private int proteinPer100g;
+
+    @Column(name = "carbs_per_100g")
+    private int carbsPer100g;
+
+    @Column(name = "fat_per_100g")
+    private int fatPer100g;
+
+    private String imageUrl;
+
+}
