4d7a480d77e3ef20af3e7d9c05f4179512533851
nhatphong5954 <pukinanasi27@gmail.com>
2025-04-05 22:17:26 +0700

    Fix api theo chuẩn response

--- Team_03/02_Source/01_Source_Code/BE/services/user-service/src/main/java/com/hcmus/userservice/dto/request/UserUpdateRequest.java
+++ Team_03/02_Source/01_Source_Code/BE/services/user-service/src/main/java/com/hcmus/userservice/dto/request/UserUpdateRequest.java
@@ -1,12 +1,13 @@
-package com.hcmus.userservice.dto;
+package com.hcmus.userservice.dto.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 
+
 @Data
+@Builder
 public class UserUpdateRequest {
     @NotBlank(message = "Name is required")
     private String name;
