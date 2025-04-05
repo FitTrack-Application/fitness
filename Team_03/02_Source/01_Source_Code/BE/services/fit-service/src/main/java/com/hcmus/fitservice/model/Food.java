432c38cad2f8cbfbab0c49d7b659fb8404874d25
binzj195 <huuphuoc6ccc@gmail.com>
2025-04-05 17:49:28 +0700

    nothing

--- Team_03/02_Source/01_Source_Code/BE/services/fit-service/src/main/java/com/hcmus/fitservice/model/Food.java
+++ Team_03/02_Source/01_Source_Code/BE/services/fit-service/src/main/java/com/hcmus/fitservice/model/Food.java
@@ -1,56 +1,25 @@
 package com.hcmus.fitservice.model;
 
 import jakarta.persistence.*;
-<<<<<<< HEAD
-import lombok.AllArgsConstructor;
-=======
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Cleanup;
->>>>>>> origin/Tu
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
-<<<<<<< HEAD
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-=======
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
->>>>>>> origin/Tu
 @Table(name = "foods")
 public class Food {
     @Id
     @GeneratedValue
     @UuidGenerator
     @Column(name = "food_id")
-<<<<<<< HEAD
-    private UUID id;
-
-    private String name;
-
-    @Column(name = "calories_per_100g")
-    private int caloriesPer100g;
-
-    @Column(name = "protein_per_100g")
-    private double proteinPer100g;
-
-    @Column(name = "carbs_per_100g")
-    private double carbsPer100g;
-
-    @Column(name = "fat_per_100g")
-    private double fatPer100g;
-
-    private String imageUrl;
-
-=======
     private UUID foodId;
 
     @NotNull(message = "Food name cannot be null")
@@ -78,5 +47,4 @@ public class Food {
     private String imageUrl;
 
     private UUID userId;
->>>>>>> origin/Tu
 }
