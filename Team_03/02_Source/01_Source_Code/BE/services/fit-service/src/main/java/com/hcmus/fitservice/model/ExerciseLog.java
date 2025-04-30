2b52131d293c7334de24d7ffa961b834ab9d261a
Tu Le <cubeaholic03@gmail.com>
2025-04-30 22:01:18 +0700

    Set up Exercises entities

---
+++ Team_03/02_Source/01_Source_Code/BE/services/fit-service/src/main/java/com/hcmus/fitservice/model/ExerciseLog.java
@@ -0,0 +1,36 @@
+package com.hcmus.fitservice.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "exercise_logs")
+public class ExerciseLog {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "exercise_log_id")
+    private UUID exerciseLogId;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @NotNull
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+}
