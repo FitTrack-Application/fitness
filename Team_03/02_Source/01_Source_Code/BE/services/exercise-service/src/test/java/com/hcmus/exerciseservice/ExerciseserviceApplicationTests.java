d5b2791359506e6014df95d8dcd9beb6fea2a730
Tu Le <cubeaholic03@gmail.com>
2025-05-21 17:54:44 +0700

    Merge bedev into dev

--- Team_03/02_Source/01_Source_Code/BE/services/exercise-service/src/test/java/com/hcmus/exerciseservice/ExerciseserviceApplicationTests.java
+++ Team_03/02_Source/01_Source_Code/BE/services/exercise-service/src/test/java/com/hcmus/exerciseservice/ExerciseserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.hcmus.exerciseservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ExerciseserviceApplicationTests {
 
 	@Test
